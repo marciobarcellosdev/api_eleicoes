5e8830e43b4885b2fca4367ba8ff14c5bcaaf1d2
Marcio Barcellos <marcbrcx@gmail.com>
2024-11-30 21:13:52 -0300

    30/11/2024 21:13

--- javascript/cfg.js
+++ javascript/cfg.js
@@ -1,2 +1,3 @@
-const host = "http://projectdev.services:3039";
+//const host = "http://projectdev.services:3039";
 //const host = "http://localhost:3039";
+const host = "http://179.217.234.187:3039";
